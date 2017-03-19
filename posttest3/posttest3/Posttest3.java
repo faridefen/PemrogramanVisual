19cbebc15838fe54acca4388bcbd5924d9f172fe
faridefen <efendyfarid@gmail.com>
2017-03-19 21:19:32 +0800

    Add files via upload

---
+++ posttest3/posttest3/Posttest3.java
@@ -0,0 +1,13 @@
+
+package posttest3;
+
+public class Posttest3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
